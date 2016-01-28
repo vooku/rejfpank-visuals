80b5cb63a86186ebcc3b5a41d60816d3fa6d63ec
Vadim Petrov <petrovad@fit.cvut.cz>
2016-01-28 14:04:24 +0100

    experimenting with windows midi library

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # REJFPANK VISUALS
 
+## Introduction
+This is my undergraduate thesis at [FIT CTU](https://www.fit.cvut.cz/en). It visualises MIDI data from incoming stream sourced from samplers, drum machines, gameboys etc.
+
+I am a big fan of what I call 'glitch aesthetics,' commonly seen in the context of the chiptune or algorave scenes. It consists of bright colors, glitches and general randomness. Expect this kind of visual turn-on.
+
+The rest of the readme is mostly for myself. 
+
 ## TODO
 - knihovny:
 	- glut vs
@@ -7,15 +14,29 @@
 		- SFML (Simple and Fast Multimedia Library)
 		- Allegro
 	- midi 
-		- Midifile
+		- [RtMidi](https://github.com/thestk/rtmidi)
 		- Juce
 		- SDL
-		- midi in 32 (midi yoke)
 
 ## Ideas
+- use cmake to create makefile
+- use a function to create VBOs
 - create an algorithm to tessellate a rectangle
 	- parameter (starting from 1) is the 'tessellation lvl' -> cell count equals 1 / (tessellation lvl + 1)^2
 - Voronoi diagram with taxicab geometry
+- Ask the good fellows over at Chiptunes = Win for some pixel artist -> Pokémon-Crystal-like sprite of a squirrel and a mutant squirrel
+- Transform pixels of BMP to vertex data and display them as points (toy with distance and point size, animate point size)
+- Fog color depending on the y coordinate
+- Switch dynamically between ortographic and perspective projection
+- Toy with glm::frustum
+- Create light emiting objects via fog
 
 ## Useful links
-http://www.upvector.com/?section=Tutorials&subsection=Intro%20to%20Procedural%20Textures
+- [Procedural textures inspiration](http://www.upvector.com/?section=Tutorials&subsection=Intro%20to%20Procedural%20Textures)
+- [Windows MIDI reference](https://msdn.microsoft.com/en-us/library/windows/desktop/dd798495(v=vs.85).aspx)
+- [openFrameworks](http://openframeworks.cc/)
+- [mTroll](http://sourceforge.net/p/mtroll/mTroll/ci/default/tree/midi/) -- windows midi example
+
+## Notes
+- Midi yoke proved to be obsolete -- incompatible with 64bit SW such as Ableton Live 9. For testing purposes I am using [loopMIDI](www.tobias-erichsen.de/software/loopmidi.html) instead.
+- OSC (Open Sound Control) is (might be) the new MIDI
--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,103 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 /**
  * @author Vadim Petrov
  * @date 2016
  */
  
-int main (void) {
+#include <conio.h>
 
+#include <iostream>
+#include <stdio.h>
+
+#include <windows.h>   /* required before including mmsystem.h */
+#include <mmsystem.h>
+
+using namespace std;
+
+bool PrintMidiDevices() {
+	unsigned int nMidiDeviceNum;
+	MIDIINCAPS caps;
+
+	nMidiDeviceNum = midiInGetNumDevs();
+	if (nMidiDeviceNum == 0) {
+		cerr << "midiInGetNumDevs() returns 0..." << endl;
+		return false;
+	}
+
+	cout << "== PrintMidiDevices() ==" << endl;
+	for (unsigned int i = 0; i < nMidiDeviceNum; ++i) {
+		midiInGetDevCaps(i, &caps, sizeof(MIDIINCAPS));
+		//cout << "\t" << i << ": name = " << caps.szPname << endl;
+		cout << "\t" << i << ":\t" << caps.wMid << ", " << caps.wPid  << ", " << caps.vDriverVersion << ", " << caps.szPname << ", " << caps.dwSupport << endl;
+	}
+	cout << endl;
+	return true;
+}
+
+void CALLBACK MidiInProc(HMIDIIN hMidiIn, UINT wMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2) {
+	switch(wMsg) {
+	case MIM_OPEN:
+		printf("wMsg=MIM_OPEN\n");
+		break;
+	case MIM_CLOSE:
+		printf("wMsg=MIM_CLOSE\n");
+		break;
+	case MIM_DATA:
+		printf("wMsg=MIM_DATA, dwInstance=%08x, dwParam1=%08x, dwParam2=%08x\n", (unsigned int) dwInstance, (unsigned int) dwParam1, (unsigned int) dwParam2);
+		break;
+	case MIM_LONGDATA:
+		printf("wMsg=MIM_LONGDATA\n"); 
+		break;
+	case MIM_ERROR:
+		printf("wMsg=MIM_ERROR\n");
+		break;
+	case MIM_LONGERROR:
+		printf("wMsg=MIM_LONGERROR\n");
+		break;
+	case MIM_MOREDATA:
+		printf("wMsg=MIM_MOREDATA\n");
+		break;
+	default:
+		printf("wMsg = unknown\n");
+		break;
+	}
+	return;
+}
+
+int main(int argc, char** argv) {
+	HMIDIIN hMidiDevice = NULL;;
+	DWORD nMidiPort = -1;
+	
+	if (midiInGetNumDevs() == 0) {
+		cerr << "midiInGetNumDevs() returns 0, quitting." << endl;
+		return -1;
+	}
+
+	if (!PrintMidiDevices()) {
+		cerr << "Cannot acces MIDI devices, quitting." << endl;
+		return -1;
+	}
+	while (nMidiPort < 0 || nMidiPort >= midiInGetNumDevs()) {
+		cout << "Enter MIDI Input device number:" << endl;
+		scanf ("%lu", &nMidiPort);
+	}
+
+	if (midiInOpen(&hMidiDevice, nMidiPort, (DWORD_PTR)MidiInProc, 0, CALLBACK_FUNCTION) != MMSYSERR_NOERROR) {
+		cout << "midiInOpen() failed, quitting." << endl;
+		return -1;
+	}
+
+	midiInStart(hMidiDevice);
+
+
+
+	midiInStop(hMidiDevice);
+	midiInClose(hMidiDevice);
+	hMidiDevice = NULL;
 
 	return 0;
 }
+
